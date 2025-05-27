09ff0957c1986f7e6abdf96082d562df915ee5dc
RanaUniverse <RanaUniverse321@gmail.com>
2025-05-27 11:44:02 +0530

    the a and b alias also has been added directly in the installation script.

--- installation_script_redshift.sh
+++ installation_script_redshift.sh
@@ -1,5 +1,7 @@
 echo "The redshift will install in this machine to control the night light."
 
- sudo dpkg -i dependencies/*.deb
+sudo dpkg -i dependencies/*.deb
 
- echo "The Redshift has been installed."
+./terminal_alias_for_redshift.sh 
+
+echo "The Redshift has been installed Successfully."
